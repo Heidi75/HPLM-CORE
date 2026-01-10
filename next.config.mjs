0ddcfea1748610906b4db8d94dd8e89a2877c421
Heidi <54006359+Heidi75@users.noreply.github.com>
2026-01-10 13:59:34 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,12 +3,6 @@ const nextConfig = {
   experimental: {
     serverActions: true,
   },
-}
-
-module.exports = nextConfig
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
   // This prevents the server from "Guessing" the route
   // and forces it to accept the POST request from your phone.
   trailingSlash: false,
